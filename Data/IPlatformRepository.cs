146b6270ab4b65e702ba91976fe78add8bf4221b
VarasMD <md.varas.arg@gmail.com>
2025-01-20 00:14:08 -0300

    build: add repository section

--- Data/IPlatformRepository.cs
+++ Data/IPlatformRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace PlatformService.Data
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
 {
     public interface IPlatformRepository
     {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform platform);
     }
 }
